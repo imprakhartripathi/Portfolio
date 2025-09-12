2570f267fe9bfe4e6dc325e61222d4a9ed1e5963
Prakhar Tripathi <imprakhartripathiofficial@gmail.com>
2025-09-12 16:39:43 +0530

    url changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 const allowedOrigins = [
   "https://imprakhartripathi.github.io", 
+  "https://imprakhartripathi.netlify.app",
   // "http://localhost:3000",               
   // "http://127.0.0.1:5500",              
   // "http://localhost:5000"                
